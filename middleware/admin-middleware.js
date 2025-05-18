bf6bc42cba1248c8b7d81f16a1a55eab001c551c
Gourav Upadhyay <99647524+tampurus@users.noreply.github.com>
2025-05-19 03:32:08 +0530

    Update admin-middleware.js

--- middleware/admin-middleware.js
+++ middleware/admin-middleware.js
@@ -1,21 +1,11 @@
 const isAdminUser = (req, res, next) => {
-    if(req.userInfo.role !== 'admin'){
+    if (req.userInfo.role !== "admin") {
         return res.status(403).json({
-            success: false,
-            message: 'Forbidden, You are not an admin user',
-        })
-    }
-    else{
-        return res.status(200).json({
-            success: true,
-            message: 'You are an admin user',
-            user: {
-                _id: req.userInfo.userId,
-                username: req.userInfo.username,
-                role: req.userInfo.role,
-            },
-        })
-    }
+          success: false,
+          message: "Access denied! Admin rights required.",
+        });
+      }
     next();
+    
 };
 module.exports = isAdminUser;
